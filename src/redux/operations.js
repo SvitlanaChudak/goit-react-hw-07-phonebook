3112efd4f3510757a83d8c36529f43afad9722cd
SvitlanaChudak <svit.lemon@gmail.com>
2023-02-24 16:07:48 +0200

    Update API

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://63f893fd5b0e4a127de8f640.mockapi.io";
+axios.defaults.baseURL = "https://63f8c3695b0e4a127ded1904.mockapi.io";
 
 export const fetchContacts = createAsyncThunk("contacts/fetchAll", async (_, thunkAPI) => {
     try {
